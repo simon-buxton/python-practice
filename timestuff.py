d1ea8a021578e7ae3d7d585d96f19df23267ed82
Simon Buxton <simon@buxton.geek.nz>
2020-05-13 12:49:55 +0100

    and fix import

--- timestuff.py
+++ timestuff.py
@@ -1,4 +1,4 @@
 from datetime import datetime, timezone
 
-#print("hello world at " + str(datetime.datetime.now()))
-print("hello world at " + datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")) 
+print("hello world at " + str(datetime.now()))
+#print("hello world at " + datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")) 
